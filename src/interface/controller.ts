d2b1552e298206ffbc83c91d43fe3bfb225cb9a8
laacin <lautarocingolani07@gmail.com>
2026-02-10 09:01:17 -0300

    test: add tmp test endpoints

--- src/interface/controller.ts
+++ src/interface/controller.ts
@@ -1,35 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { XlsxUseCase } from 'src/app/usecases.app';
-import { Format } from 'src/domain/format';
 
 const format = '{"name": "String","age": "Number","nums": "array<number>"}';
-const fmt = new Format(format);
 
 @Controller()
 export class XlsxController {
   constructor(private readonly use: XlsxUseCase) {}
 
-  @Get('/errs')
-  getReadWithHeaders() {
-    const response = this.use.uploadTest(
-      process.env.TEST_FILE_WITH_ERRORS!,
-      fmt,
-    );
-    return response;
+  @Get('/process/:id')
+  async getReadWithHeaders(@Param('id') id: string) {
+    const res = await this.use.processData(id);
+    return res;
   }
 
-  @Get('/extra')
-  getReadWithExtra() {
-    const response = this.use.uploadTest(
-      process.env.TEST_FILE_WITH_EXTRA!,
-      fmt,
+  @Get('/status/:id')
+  async getReadWithExtra(@Param('id') id: string) {
+    const res = await this.use.checkJobStatus(id);
+    return res;
+  }
+
+  @Get('/upload')
+  async getReadTest() {
+    const res = await this.use.uploadFile(
+      process.env.TEST_FILE_WITH_ERRORS!,
+      format,
     );
-    return response;
+    return res;
   }
 
   @Get()
-  getReadTest() {
-    const response = this.use.uploadTest(process.env.TEST_FILE!, fmt);
-    return response;
+  getHello() {
+    return 'hello';
   }
 }
