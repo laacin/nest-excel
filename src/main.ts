57285607d516047bf5987685693343d4b80a344d
laacin <lautarocingolani07@gmail.com>
2026-02-20 16:04:07 -0300

    feat: add docker-based initialization

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { MainModule } from 'src/main.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(await MainModule.forRootAsync({}));
+  const mod = await MainModule.forRootAsync({
+    infra: {
+      /// docker config
+      mongo: { host: 'mongo', port: 27017, db: 'mydb' },
+      amqp: { host: 'amqp', port: 5672, user: 'guest', pass: 'guest' },
+
+      /// local config
+      // mongo: { host: 'localhost', port: 27017, db: 'mydb' },
+      // amqp: { host: 'localhost', port: 5672, user: 'guest', pass: 'guest' },
+    },
+  });
+
+  const app = await NestFactory.create(mod);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((e) => console.error(e));
